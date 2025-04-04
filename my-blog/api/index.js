dec7903be1ff2009a6481c85d81ca32913b9933f
Usman Shahid <usmanshahid027@outlook.com>
2025-04-04 16:22:30 +0500

    created auth route with user saving in database

--- my-blog/api/index.js
+++ my-blog/api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -19,9 +20,11 @@ try {
 }
 
 const app = express();
+app.use(express.json()); // this will enable json receiving in the app
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes)
