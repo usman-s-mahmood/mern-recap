5226294844094372be5f83c72fb02c6461efa4ce
Usman Shahid <usmanshahid027@outlook.com>
2025-04-04 15:08:03 +0500

    connected to mongo DB cloud

--- my-blog/api/index.js
+++ my-blog/api/index.js
@@ -1,4 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+try {
+    mongoose.connect(
+        process.env.MONGO // 'mongodb+srv://use_your_username:use_your_password@cluster0.uwvx8w4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    ).then(() => {
+        console.log('MongoDB Connected!')
+    }).catch((err) => {
+        console.error(`Unable to connect with MongoDB Cloud`);
+    });
+} catch(error) {
+    console.error('Unable to Connect with MongoDB!')
+    exit(1)
+}
 
 const app = express();
 
