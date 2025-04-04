7ae8dde0a77ab50f8aa6ea8c60202a04948fec0a
Usman Shahid <usmanshahid027@outlook.com>
2025-04-04 17:28:09 +0500

    did working on sign up UI

--- my-blog/client/src/pages/SignUp.jsx
+++ my-blog/client/src/pages/SignUp.jsx
@@ -1,10 +1,63 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-      <p className="text-xl text-red-400">This is Register Page</p>
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-3 flex-center">
+        <div className="flex-1"> 
+          <Link to='/' className='text-lg font-bold dark:text-white'>
+            <span className='px-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white mx-2'>MY</span>Blog
+          </Link>
+          <p>A demo blog project based on MERN stack!</p>
+        </div>
+        <div className="flex-1">
+          <form className='flex flex-col gap-4'>
+            <div className="">
+              <label htmlFor="username">your username</label>
+              <TextInput 
+                type='text'
+                placeholder='Enter your username'
+                id='username'
+              />
+            </div>
+            <div className="">
+              <label htmlFor="email">your email</label>
+              <TextInput 
+                type='email'
+                placeholder='Enter your email'
+                id='email'
+              />
+            </div>
+            <div className="">
+              <label htmlFor="password">your password</label>
+              <TextInput 
+                type='password'
+                placeholder='Enter your password'
+                id='password'
+              />
+            </div>
+            <div className="">
+              <label htmlFor="confirm_password">confirm password</label>
+              <TextInput 
+                type='password'
+                placeholder='Confirm your password'
+                id='confirm_password'
+              />
+            </div>
+            <Button  
+              className='bg-gradient-to-r from-purple-300 via-pink-500 to-purple-700 hover:cursor-pointer'
+              pill
+              type='submit'
+            >Submit</Button>
+          </form>
+          <div className='flex gap-1 text-sm mt-5'>
+            <span>Already Have an account?</span>
+            <Link to={'/sign-in'} className='text-blue-500'>Sign In</Link>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
